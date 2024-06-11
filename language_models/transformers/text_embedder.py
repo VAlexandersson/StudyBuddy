5bbda770ee4e21f9187d7a7c4e6a8d8b9647d0cd
Viktor Alexandersson <viktor.alexand@gmail.com>
2024-06-11 13:07:37 +0200

    refactor

--- language_models/transformers/text_embedder.py
+++ language_models/transformers/text_embedder.py
@@ -1,5 +1,5 @@
 from sentence_transformers import SentenceTransformer
-from interfaces.base_models import EmbeddingModelInterface
+from interfaces.language_models import EmbeddingModelInterface
 
 class TextEmbedder(EmbeddingModelInterface):
   def __init__(self):
